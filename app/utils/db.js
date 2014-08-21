b6621a90f7874bdcd969cb235845580c8273755d
Selva <selva@selvakn.in>
2014-08-21 18:15:18 +0000

    Use rediscloud in heroku

--- app/utils/db.js
+++ app/utils/db.js
@@ -1,8 +1,18 @@
 'use strict';
 
 var redis = require('redis'),
-  wrapper = require('co-redis');
+  wrapper = require('co-redis'),
+  url = require('url');
 
-var db = wrapper(redis.createClient(6379, '127.0.0.1', {}));
+var client;
 
-module.exports = db;
+if(process.env.REDISCLOUD_URL) {
+  var redisURL = url.parse(process.env.REDISCLOUD_URL);
+  client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+  client.auth(redisURL.auth.split(":")[1]);
+} else {
+  client = redis.createClient(6379, '127.0.0.1', {})
+}
+
+module.exports = wrapper(client);
+
